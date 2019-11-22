3eb4cf7a628c467cf52a727539c730b4ea7fa905
Rajnesh Thakur <rajneshthakur0000@gmail.com>
2019-11-22 18:05:29 +0530

    More comments

--- SockMerchantSolution.java
+++ SockMerchantSolution.java
@@ -23,11 +23,11 @@ public class SockMerchantSolution {
 		}
 		return matchCount;
 	}
-
+	//Scanner InstantiationException
 	private static final Scanner scanner = new Scanner(System.in);
 	//Main Method
 	public static void main(String[] args) throws IOException {
-
+		//Inside Main Method
 		int n = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
